551e25d0b542e9bff5f9382b0831025c4cc3e093
AbdlerahmanAbdelrazek <abdou007.abdelrahman@gmail.com>
2016-02-19 00:01:52 +0200

    added comments to pages "again" Hossam added the same commit but i accendtly removed his changes in later commits.

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     end
   end
 
+   resources :pages do
+    resources :comments
+    end
+
   root "documents#index"
 
   resources :users, :except => [:index] do
