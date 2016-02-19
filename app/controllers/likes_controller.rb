34d05f2668f16be3027ce384118502ab6b2dde68
Essam <essameid614@gmail.com>
2016-02-19 10:05:47 +0200

    finished controller remaining adding the likes to views and controllers : pages and documents

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,33 @@
 class LikesController < ApplicationController
 before_filter :load_likable
+
+def index
+	@likes = @likable.likes
+	@number_of_likes = @likes.size
+end
+
+def new
+	 
+	if @likes.find_by user_id: session[:user_id]
+		redircet_to(:action => delete)
+	else
+	@like = Like.new(params_like)
+	@like.save
+end
+
+def delete
+	@likes.find_by {user_id: session[:user_id]}.destroy
+end
+
+
 private
 def load_likable
 resource, id = request.path.split('/'[1,2])
 @likable = resource.singularize.classify.constantize.find(id)
 end
+
+def params_like
+	params.require(:like).permit(:likable,:likable_id,:user_id)
+end
+
 end
